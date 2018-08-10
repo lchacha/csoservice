952fba3759faa6b6e3e79d8e66af22c720140d40
lchacha <lenah90ke@gmail.com>
2018-08-10 14:58:38 -0700

    Added buttons to the datatables

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,12 @@
-
- var editor
+var contactEditor;
 var appname = "";
 var appName = ""
 $(document).ready(function() {
-	
-
 
 	  $('#addPerson').click(function(){
         	$('#dynamic_content').load('/contactcsos')
     });
+
+	
 });
 	
