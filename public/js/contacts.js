7b3c19d62ccddf2e7b022240d462f1e4c19d8b7f
lchacha <lenah90ke@gmail.com>
2018-08-15 05:46:39 -0700

    added person form

---
+++ public/js/contacts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ 
+ $('#saveContact').click(function(){
+ // get the values from the form
+ var datastring = $('#newPerson').serialize();
+ alert(datastring)
+ // save the record to database
+ // on success -> save to table
+})                                                                                                                
+ 
+ })      
+
