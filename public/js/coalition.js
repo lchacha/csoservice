fecb890326ec713485b8761eff594973f85869e1
Sharafa <nsharafa@gmail.com>
2018-09-05 01:59:02 +0300

    Added coalition form

--- public/js/coalition.js
+++ public/js/coalition.js
@@ -2,4 +2,14 @@
       window.init_SmartWizard();
       window.init_wysiwyg();
       window.init_TagsInput();
+
+      // function that handles when the form is submitted
+      $('.buttonFinish').click(function(e){
+            e.preventDefault()
+            messageData={}
+
+            // now collect all the fields from the foem
+            messageData.name=$('#inputSuccess1').val().trim()
+            messageData.name=$('#inputSuccess2').val().trim()
+      })
 })
